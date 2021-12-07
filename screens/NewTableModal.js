d4da67dbb5aa73104dccffd151d8c7f4a92138bd
amitrohan <amit@kruzr.co>
2021-12-07 14:53:14 +0530

    Added Base UI

--- screens/NewTableModal.js
+++ screens/NewTableModal.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import {Text, View, Button} from 'react-native';
 
-function NewItemsModal({navigation}) {
+function NewTableModal({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>NewItemsModal!</Text>
+      <Text style={{fontSize: 30}}>NewTableModal!</Text>
       <Button onPress={() => navigation.goBack()} title="Dismiss" />
     </View>
   );
 }
 
-export default NewItemsModal;
+export default NewTableModal;
