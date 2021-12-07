b11196198256a40e1f653ded1af0a81daa52eb0d
amitrohan <amit@kruzr.co>
2021-12-07 18:01:44 +0530

    Added items to state and basic ui for list

--- screens/ItemsScreen.js
+++ screens/ItemsScreen.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import {Text, View, Button} from 'react-native';
+import {ItemStore} from '../utils/ItemStore';
 
 function ItemsScreen({navigation}) {
+  const [items, setItems] = React.useState([]);
+
+  const getAllItems = () => {
+    return items.map((item, pos) => <Text key={pos}>{item.name}</Text>);
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>ItemsScreen!</Text>
+      {getAllItems()}
       <Button
         onPress={() => navigation.navigate('New Item', {msg: 'HI'})}
         title="Add Item"
