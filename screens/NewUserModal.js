72def5eb525386ba13c12bcc9dd64ffbc1bb886e
amitrohan <amit@kruzr.co>
2021-12-07 20:13:03 +0530

    Updated FN to sotre User Details to DB

--- screens/NewUserModal.js
+++ screens/NewUserModal.js
@@ -1,11 +1,54 @@
 import * as React from 'react';
-import {Text, View, Button} from 'react-native';
+import {View} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
+import {UserStore} from '../utils/UserStore';
 
 function NewUserModal({navigation}) {
+  const userNameInputRef = React.useRef();
+  const userNumberInputRef = React.useRef();
+  const addUserButtonRef = React.useRef();
+  const [userName, setUserName] = React.useState('');
+  const [userNumber, setUserNumber] = React.useState(0);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>NewUserModal!</Text>
-      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    <View>
+      <TextInput
+        ref={userNameInputRef}
+        autoFocus={true}
+        placeholder="Name"
+        label="Name"
+        mode="outlined"
+        onChangeText={text => setUserName(text)}
+        onSubmitEditing={() => {
+          userNumberInputRef.current.focus();
+        }}
+        blurOnSubmit={false}
+      />
+      <TextInput
+        ref={userNumberInputRef}
+        keyboardType="numeric"
+        mode="outlined"
+        label="Mobile Number"
+        placeholder="Mobile Number"
+        onChangeText={text => setUserNumber(text)}
+      />
+      <Button
+        ref={addUserButtonRef}
+        mode="contained"
+        onPress={() => {
+          if (userName.trim().length === 0) {
+            userNameInputRef.current.focus();
+            return;
+          }
+          UserStore.addItem({name: userName, number: userNumber}).finally(_ => {
+            navigation.goBack();
+          });
+        }}>
+        Add
+      </Button>
+      <Button mode="outlined" onPress={() => navigation.goBack()}>
+        Dismiss
+      </Button>
     </View>
   );
 }
