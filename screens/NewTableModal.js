f26f9f22f2d9c91f5995b9fc03d541a048521ce7
amitrohan <amit@kruzr.co>
2021-12-08 02:56:33 +0530

    Added Basic Table Addition

--- screens/NewTableModal.js
+++ screens/NewTableModal.js
@@ -1,48 +1,108 @@
 import * as React from 'react';
-import {Text, View, Button, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Platform} from 'react-native';
 import Autocomplete from 'react-native-autocomplete-input';
-import {TextInput} from 'react-native-paper';
+import {Text, TextInput, Button} from 'react-native-paper';
+import {TableStore} from '../utils/TableStore';
+import {UserStore} from '../utils/UserStore';
 
 function NewTableModal({navigation}) {
-  
-  const filterData = dataToSearch => {
-    return allItems.filter(item => item.name.indexOf(dataToSearch) !== -1);
+  const [users, setUsers] = React.useState([]);
+  const userNameInputRef = React.useRef();
+
+  React.useEffect(() => {
+    // ON PAGE LOAD
+    const unsubscribe = navigation.addListener('focus', () => {
+      UserStore.getItems()
+        .then((_users = []) => {
+          setUsers(_users);
+        })
+        .catch(_ => setUsers([]));
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  const filterUserName = nameToSearch => {
+    return users.filter(item => item.name.indexOf(nameToSearch) !== -1);
   };
 
-  const [query, setQuery] = React.useState('');
-  const [allItems, setAllMovies] = React.useState([]);
-  const filteredData = filterData(query);
+  const [userNameQuery, serUserNameQuery] = React.useState('');
+  const filteredData = filterUserName(userNameQuery);
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.autocompleteContainer}>
         <Autocomplete
+          ref={userNameInputRef}
           autoCorrect={false}
+          autoFocus={true}
           data={
-            filteredData?.length === 1 && filteredData[0].compareTitle(query)
-              ? [] // Close suggestion list in case movie title matches query
+            filteredData?.length === 1 && filteredData[0].name === userNameQuery
+              ? []
               : filteredData
           }
-          onChangeText={setQuery}
-          // renderTextInput={TextInput}
+          value={userNameQuery}
+          onChangeText={serUserNameQuery}
+          mode="outlined"
+          label="User Name"
+          placeholder="Name"
+          renderTextInput={props => <TextInput {...props} />}
           flatListProps={{
             keyExtractor: (_, idx) => idx,
-            renderItem: ({item}) => <Text>{item}</Text>,
+            renderItem: ({item: {name}}) => (
+              <TouchableOpacity onPress={() => serUserNameQuery(name)}>
+                <Text style={styles.itemText}>{name}</Text>
+              </TouchableOpacity>
+            ),
           }}
         />
       </View>
 
-      {/* <Button onPress={() => navigation.goBack()} title="Dismiss" /> */}
+      <View style={styles.restContent}>
+        <Button
+          mode="contained"
+          onPress={() => {
+            if (userNameQuery.trim().length === 0) {
+              userNameInputRef.current.focus();
+              return;
+            }
+            TableStore.addItem({
+              name: userNameQuery,
+              active: true,
+              items: [],
+            }).finally(_ => navigation.goBack());
+          }}>
+          Add
+        </Button>
+        <Button mode="outlined" onPress={() => navigation.goBack()}>
+          Dismiss
+        </Button>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    flex: 1,
+    // Android requiers padding to avoid overlapping
+    // with content and autocomplete
+    paddingTop: 50,
+  },
+  restContent: {
+    marginTop: 8,
+    zIndex: -1,
+  },
   autocompleteContainer: {
     flex: 1,
     left: 0,
     position: 'absolute',
     right: 0,
     top: 0,
-    zIndex: 1,
+    zIndex: 99999,
+  },
+  itemText: {
+    fontSize: 15,
+    margin: 2,
   },
 });
 export default NewTableModal;
