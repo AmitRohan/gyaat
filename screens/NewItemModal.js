3cc74e57d6b6be3b8978de6b59fcaddd9a390376
amitrohan <amit@kruzr.co>
2021-12-07 15:29:44 +0530

    Added input for Item Name & Price

--- screens/NewItemModal.js
+++ screens/NewItemModal.js
@@ -1,10 +1,40 @@
 import * as React from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, TextInput} from 'react-native';
 
 function NewItemModal({navigation}) {
+  const itemPriceInputRef = React.useRef();
+  const addItemButtonRef = React.useRef();
+  const [itemName, setItemName] = React.useState('');
+  const [itemPrice, setItemPrice] = React.useState(0);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{fontSize: 30}}>NewItemsModal!</Text>
+      <TextInput
+        autoFocus={true}
+        style={{height: 40}}
+        onKe
+        placeholder="Name"
+        onChangeText={text => setItemName(text)}
+        onSubmitEditing={() => {
+          itemPriceInputRef.current.focus();
+        }}
+        blurOnSubmit={false}
+      />
+      <TextInput
+        ref={itemPriceInputRef}
+        style={{height: 40}}
+        keyboardType="numeric"
+        placeholder="Price"
+        onChangeText={text => setItemPrice(text)}
+      />
+      <Button
+        ref={addItemButtonRef}
+        onPress={() => {
+          console.log('Item Added', itemName, itemPrice);
+          navigation.goBack();
+        }}
+        title="Add"
+      />
       <Button onPress={() => navigation.goBack()} title="Dismiss" />
     </View>
   );
