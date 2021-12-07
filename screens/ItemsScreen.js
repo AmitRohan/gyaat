9ae20c3bcd4b9f12602e090889c7ad921589b4f1
amitrohan <amit@kruzr.co>
2021-12-07 18:15:16 +0530

    Switched to React FlatList

--- screens/ItemsScreen.js
+++ screens/ItemsScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, StyleSheet, FlatList} from 'react-native';
 import {ItemStore} from '../utils/ItemStore';
 
 function ItemsScreen({navigation}) {
@@ -19,18 +19,38 @@ function ItemsScreen({navigation}) {
   }, [navigation]);
 
   const getAllItems = () => {
-    return items.map((item, pos) => <Text key={pos}>{item.name}</Text>);
+    return (
+      <FlatList
+        data={items}
+        renderItem={({item}) => {
+          console.log(item);
+          return <Text style={styles.listItem}>{item.name}</Text>;
+        }}
+      />
+    );
   };
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {getAllItems()}
+    <View style={styles.container}>
       <Button
         onPress={() => navigation.navigate('New Item', {msg: 'HI'})}
         title="Add Item"
       />
+      {getAllItems()}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  listItem: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
 export default ItemsScreen;
