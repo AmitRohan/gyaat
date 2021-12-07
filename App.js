d8f541c3d2f749dce3f188874f3b6e6a00279dcc
amitrohan <amit@kruzr.co>
2021-12-07 18:57:12 +0530

    Added Base Theme

--- App.js
+++ App.js
@@ -8,10 +8,20 @@
 
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Image} from 'react-native';
+import {
+  Provider as PaperProvider,
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
 import DashboardScreen from './screens/DashboardScreen';
+import merge from 'deepmerge';
 
 import {
   ItemsScreenNavigations,
@@ -27,62 +37,68 @@ const usersIcon = require('./assets/icons/users.png');
 const Tab = createBottomTabNavigator();
 const tabBarActiveTintColor = 'green';
 const tabBarInactiveTintColor = 'red';
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          headerShown: route.name === 'Dashboard', // SHOW ONLY FOR DASHBOARD
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            let tintColor;
-            if (focused) {
-              tintColor = tabBarActiveTintColor;
-            } else {
-              tintColor = tabBarInactiveTintColor;
-            }
+    <PaperProvider theme={CombinedDefaultTheme}>
+      <NavigationContainer theme={CombinedDefaultTheme}>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            headerShown: route.name === 'Dashboard', // SHOW ONLY FOR DASHBOARD
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+              let tintColor;
+              if (focused) {
+                tintColor = tabBarActiveTintColor;
+              } else {
+                tintColor = tabBarInactiveTintColor;
+              }
 
-            switch (route.name) {
-              case 'Tables':
-                iconName = tablesIcon;
-                break;
-              case 'Items':
-                iconName = itemsIcon;
-                break;
-              case 'Users':
-                iconName = usersIcon;
-                break;
-              case 'Dashboard':
-                iconName = dashboardIcon;
-                break;
-            }
+              switch (route.name) {
+                case 'Tables':
+                  iconName = tablesIcon;
+                  break;
+                case 'Items':
+                  iconName = itemsIcon;
+                  break;
+                case 'Users':
+                  iconName = usersIcon;
+                  break;
+                case 'Dashboard':
+                  iconName = dashboardIcon;
+                  break;
+              }
 
-            return (
-              <Image
-                source={iconName}
-                tintColor={tintColor}
-                style={{
-                  width: size,
-                  height: size,
-                  borderRadius: size,
-                  tintColor: tintColor,
-                }}
-              />
-            );
-          },
-          tabBarActiveTintColor: tabBarActiveTintColor,
-          tabBarInactiveTintColor: tabBarInactiveTintColor,
-        })}>
-        <Tab.Screen name="Tables" component={TableScreenNavigations} />
-        <Tab.Screen name="Items" component={ItemsScreenNavigations} />
-        <Tab.Screen name="Users" component={UsersScreenNavigations} />
-        <Tab.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          // options={{tabBarBadge: 3}}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+              return (
+                <Image
+                  source={iconName}
+                  tintColor={tintColor}
+                  style={{
+                    width: size,
+                    height: size,
+                    borderRadius: size,
+                    tintColor: tintColor,
+                  }}
+                />
+              );
+            },
+            tabBarActiveTintColor: tabBarActiveTintColor,
+            tabBarInactiveTintColor: tabBarInactiveTintColor,
+          })}>
+          <Tab.Screen name="Tables" component={TableScreenNavigations} />
+          <Tab.Screen name="Items" component={ItemsScreenNavigations} />
+          <Tab.Screen name="Users" component={UsersScreenNavigations} />
+          <Tab.Screen
+            name="Dashboard"
+            component={DashboardScreen}
+            // options={{tabBarBadge: 3}}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
