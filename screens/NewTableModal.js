96b9605052426755e3775017e22e6fe7fef085d7
amitrohan <amit@kruzr.co>
2021-12-08 02:26:13 +0530

    Added AutoTextComplete

--- screens/NewTableModal.js
+++ screens/NewTableModal.js
@@ -1,13 +1,48 @@
 import * as React from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, StyleSheet} from 'react-native';
+import Autocomplete from 'react-native-autocomplete-input';
+import {TextInput} from 'react-native-paper';
 
 function NewTableModal({navigation}) {
+  
+  const filterData = dataToSearch => {
+    return allItems.filter(item => item.name.indexOf(dataToSearch) !== -1);
+  };
+
+  const [query, setQuery] = React.useState('');
+  const [allItems, setAllMovies] = React.useState([]);
+  const filteredData = filterData(query);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>NewTableModal!</Text>
-      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    <View>
+      <View style={styles.autocompleteContainer}>
+        <Autocomplete
+          autoCorrect={false}
+          data={
+            filteredData?.length === 1 && filteredData[0].compareTitle(query)
+              ? [] // Close suggestion list in case movie title matches query
+              : filteredData
+          }
+          onChangeText={setQuery}
+          // renderTextInput={TextInput}
+          flatListProps={{
+            keyExtractor: (_, idx) => idx,
+            renderItem: ({item}) => <Text>{item}</Text>,
+          }}
+        />
+      </View>
+
+      {/* <Button onPress={() => navigation.goBack()} title="Dismiss" /> */}
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  autocompleteContainer: {
+    flex: 1,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    zIndex: 1,
+  },
+});
 export default NewTableModal;
