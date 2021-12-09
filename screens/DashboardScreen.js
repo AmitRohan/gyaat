1f3a55215bcb0c350bbf904d72d0de89da922b15
amitrohan <amit@kruzr.co>
2021-12-09 12:32:44 +0530

    Added Tables to state

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,7 +1,23 @@
 import * as React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {TableStore} from '../utils/TableStore';
 
-function DashboardScreen() {
+function DashboardScreen({navigation}) {
+  const [tables, setTables] = React.useState([]);
+
+  React.useEffect(() => {
+    // ON PAGE LOAD
+    const unsubscribe = navigation.addListener('focus', () => {
+      TableStore.getItems()
+        .then((_tables = []) => {
+          setTables(_tables.filter(table => table.active));
+        })
+        .catch(_ => setTables([]));
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <Text>DashboardScreen!</Text>
