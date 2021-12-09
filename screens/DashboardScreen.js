8858e81afd20467e0dfe39e6d748eefd1ce7d964
amitrohan <amit@kruzr.co>
2021-12-09 13:34:08 +0530

    Added sample distribution pie chart

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
-import { Text } from 'react-native-paper';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {PieChart} from 'react-native-chart-kit';
+import {Text} from 'react-native-paper';
 import {TableStore} from '../utils/TableStore';
+import {paletteAll} from '../utils/ColorPallete';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 function DashboardScreen({navigation}) {
   const [tables, setTables] = React.useState([]);
@@ -19,9 +24,72 @@ function DashboardScreen({navigation}) {
     return unsubscribe;
   }, [navigation]);
 
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2,
+    useShadowColorFromDataset: false,
+  };
+  const getItemDistributionUI = () => {
+    let allOrders = [];
+    tables.map(table => {
+      allOrders = allOrders.concat(table.orders);
+    });
+    const dataSet = allOrders
+      .map(order => {
+        return {
+          name: order.item.name,
+          quantity: order.quantity,
+          color: '#ff0066',
+        };
+      })
+      .reduce((pv, cv) => {
+        var nV = Object.assign([], pv);
+        var isNew = true;
+        pv.forEach((v, pos) => {
+          if (v.name === cv.name) {
+            var modifiedEntry = Object.assign({}, v);
+            modifiedEntry.quantity = parseInt(modifiedEntry.quantity) + parseInt(cv.quantity);
+            nV[pos] = modifiedEntry;
+            isNew = false;
+          }
+        });
+        if (isNew) {
+          nV.push(cv);
+        }
+        return nV;
+      }, [])
+      .map(({name, quantity}, pos) => {
+        return {
+          name,
+          quantity,
+          color: paletteAll[pos % paletteAll.length],
+        };
+      });
+    return (
+      <View>
+        <Text>Item Distribution</Text>
+        <PieChart
+          data={dataSet}
+          width={windowWidth}
+          height={220}
+          chartConfig={chartConfig}
+          accessor={'quantity'}
+          backgroundColor={'transparent'}
+          paddingLeft={'15'}
+          absolute
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text>DashboardScreen!</Text>
+      {getItemDistributionUI()}
     </View>
   );
 }
